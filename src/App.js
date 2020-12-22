e4370a329f4e9929d32eeb7cec02541f042231a3
misterblack0 <misterblack0@protonmail.com>
2020-12-22 18:00:42 +0200

    list updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 import Details from "./components/Details/Details";
+import Main from "./components/Main/Main";
 import useStyles from "./styles";
 
 const App = () => {
@@ -18,8 +19,8 @@ const App = () => {
                 <Grid item xs={12} sm={4}>
                     <Details title="Income" />
                 </Grid>
-                <Grid item xs={12} sm={4}>
-                    Main
+                <Grid item xs={12} sm={3}>
+                    <Main />
                 </Grid>
                 <Grid item xs={12} sm={4}>
                     <Details title="Expense" />
