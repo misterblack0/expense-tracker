5106c42a3a87b26c3633cd6b1ff9a44b6d531875
misterblack0 <misterblack0@protonmail.com>
2020-12-24 21:55:50 +0200

    useTransactions logic

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Card, CardHeader, CardContent, Typography } from "@material-ui/core";
-/* import { Doughnut } from "react-chartjs-2"; */
+import { Doughnut } from "react-chartjs-2";
 import useStyles from "./styles";
+import useTransactions from "../../useTransactions";
 import PropTypes from "prop-types";
 
 const Details = ({ title }) => {
@@ -10,13 +11,14 @@ const Details = ({ title }) => {
     };
 
     const classes = useStyles();
+    const { total, chartData } = useTransactions(title);
 
     return (
         <Card className={title === "Income" ? classes.income : classes.expense}>
             <CardHeader title={title} />
             <CardContent>
-                <Typography variant="h5">$50</Typography>
-                {/* <Doughnut data="DATA" /> */}
+                <Typography variant="h5">${total}</Typography>
+                <Doughnut data={chartData} />
             </CardContent>
         </Card>
     );
